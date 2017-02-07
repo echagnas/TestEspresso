843956a68574fef4d64768de2046d7f0f60dc7d0
Emmanuel Chagnas <emmanuelchagnas@gmail.com>
2017-02-07 16:14:25 +0100

    First commit

---
+++ app/src/androidTest/java/test/manu/com/testespresso/ExampleInstrumentedTest.java
@@ -0,0 +1,43 @@
+package test.manu.com.testespresso;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("test.manu.com.testespresso", appContext.getPackageName());
+
+        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.activity_main_title)).check(matches(withText("Hello World!")));
+    }
+}
